4650b464f3068d0f427ef7bd4733b52a13db59da
Adam-Schapekahm <60486516+Adam-Schapekahm@users.noreply.github.com>
2021-09-05 17:18:29 -0500

    add try catch

--- app/src/main/java/edu/ags/timedifference/MainActivity.java
+++ app/src/main/java/edu/ags/timedifference/MainActivity.java
@@ -43,50 +43,54 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
 
-                //Gather input for StartTime
-                String startTime = etInput.getText().toString();
-                String[] parts = startTime.split(":");
+                try {
 
-                int hr = Integer.parseInt(parts[0]);
-                int min = Integer.parseInt(parts[1]);
-                int sec = Integer.parseInt(parts[2]);
+                    //Gather input for StartTime
+                    String startTime = etInput.getText().toString();
+                    String[] parts = startTime.split(":");
 
-                int startSeconds = hr * 3600 + min * 60 + sec;
+                    int hr = Integer.parseInt(parts[0]);
+                    int min = Integer.parseInt(parts[1]);
+                    int sec = Integer.parseInt(parts[2]);
+                    int startSeconds = hr * 3600 + min * 60 + sec;
+                    
+                    //Gather input for endTime
+                    String endTime = etInput2.getText().toString();
+                    String[] parts2 = endTime.split(":");
 
+                    int hr2 = Integer.parseInt(parts2[0]);
+                    int min2 = Integer.parseInt(parts2[1]);
+                    int sec2 = Integer.parseInt(parts2[2]);
 
-                //Gather intput for endTime
-                String endTime = etInput2.getText().toString();
-                String[] parts2 = endTime.split(":");
+                    int endSeconds = hr2 * 3600 + min2 * 60 + sec2;
 
-                int hr2 = Integer.parseInt(parts2[0]);
-                int min2 = Integer.parseInt(parts2[1]);
-                int sec2 = Integer.parseInt(parts2[2]);
+                    Log.d(TAG, "onCreate: " + startSeconds);
 
-                int endSeconds = hr2 * 3600 + min2 * 60 + sec2;
+                    //Get the absolute value of the difference between the two numbers
+                    int secondsDif = abs(startSeconds - endSeconds);
 
-                Log.d(TAG, "onCreate: " + startSeconds);
+                    //calculate the hours,minutes and seconds for result
+                    int p1 = secondsDif % 60;
+                    int p2 = secondsDif / 60;
+                    int p3 = p2 % 60;
+                    p2 = p2 / 60;
 
-                //Get the absolute value of the difference between the two numbers
-                int secondsDif = abs(startSeconds - endSeconds);
+                    //Add the leading 0s
+                    String padded1 = String.format("%02d", p1);
+                    String padded2 = String.format("%02d", p2);
+                    String padded3 = String.format("%02d", p3);
 
-                //calculate the hours,minutes and seconds for result
-                int p1 = secondsDif % 60;
-                int p2 = secondsDif / 60;
-                int p3 = p2 % 60;
-                p2 = p2 / 60;
+                    // Display the text in the textview
 
-                //Add the leading 0s
-                String padded1 = String.format("%02d",p1);
-                String padded2 = String.format("%02d",p2);
-                String padded3 = String.format("%02d",p3);
+                    String message = secondsDif + " = " + padded2 + ":" + padded3 + ":" + padded1;
+                    tvOutput.setText(message);
 
-                // Display the text in the textview
+                    Log.d(TAG, "onClick: " + message);
+                    showMessage(message);
+                } catch (Exception ex) {
+                    tvOutput.setText("Bad Input Please try again");
+                }
 
-                String message = secondsDif + " = " + padded2 + ":" + padded3 + ":" + padded1 ;
-                tvOutput.setText(message);
-
-                Log.d(TAG, "onClick: " + message);
-                showMessage(message);
             }
         });
     }
